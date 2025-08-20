82842182c1cb514f2cfc4a34bc0ed41b594b805e
kbrummitt <kbrummitt@spidersecurity.io>
2025-08-19 20:41:57 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/spidersecurity-landing/",   // <— MUST match your repo name
+  base: "/spidersecurity-landing/",  // MUST match the repo name
   plugins: [react()],
 })
